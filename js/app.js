52b9d6658b2f8c1d5787d66f60ad70903eeda01c
Erin Meyers <erinmeyers88@gmail.com>
2015-11-02 13:02:23 -0700

    Finished basic functionality.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var app = angular.module("miniRouting", ["ui.router"]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+		.state("home", {
+
+			templateUrl: "js/home/homeTmpl.html",
+			controller: "homeCtrl",
+			url: "/"
+		})
+
+		.state("settings", {
+			templateUrl: "js/settings/settingsTmpl.html",
+			controller: "settingsCtrl",
+			url: "/settings"
+		})
+
+		.state("products", {
+			templateUrl: "js/products/productTmpl.html",
+			controller: "productsCtrl",
+			url: "/products/:id"
+		})
+
+	$urlRouterProvider.otherwise("/");
+});
